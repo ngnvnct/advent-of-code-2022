c7f3381f54439e33985201d3eb5ae49a9863fe89
Vincent Nguyen <ngn.vincent@gmail.com>
2022-12-04 00:43:25 -0800

    a little more readable but still jank

--- day4/day4 part 1.py
+++ day4/day4 part 1.py
@@ -3,7 +3,8 @@ def main():
     with open("day4\input.txt") as r:
         for line in r:
             first, second = list(map(int, (line.split(",")[0]).split("-"))), list(map(int, (line.split(",")[1].strip()).split("-")))    # Returns list of string-to-int elements
-            if (first[0] >= second[0] and first[1] <= second[1]) or (second[0] >= first[0] and second[1] <= first[1]):
+            if (second[0] >= first[0] and second[1] <= first[1]) or (
+                first[0] >= second[0] and first[1] <= second[1]):
                 amount += 1
     print(amount)
 
